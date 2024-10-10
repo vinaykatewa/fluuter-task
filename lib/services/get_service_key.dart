91b5313b31023837bcb434200ac1b7ca7c75258c
vinaykatewa <imvinaykatewa@gmail.com>
2024-10-10 19:47:19 +0530

    initial

--- lib/services/get_service_key.dart
+++ lib/services/get_service_key.dart
@@ -8,5 +8,23 @@ class GetServerKey {
       'https://www.googleapis.com/auth/firebase.database',
       'https://www.googleapis.com/auth/firebase.messaging',
     ];
+    final client = await clientViaServiceAccount(
+      ServiceAccountCredentials.fromJson({
+        "type": dotenv.env['TYPE'],
+        "project_id": dotenv.env['PROJECT_ID'],
+        "private_key_id": dotenv.env['PRIVATE_KEY_ID'],
+        "private_key": dotenv.env['PRIVATE_KEY'],
+        "client_email": dotenv.env['CLIENT_EMAIL'],
+        "client_id": dotenv.env['CLIENT_ID'],
+        "auth_uri": dotenv.env['AUTH_URI'],
+        "token_uri": dotenv.env['TOKEN_URI'],
+        "auth_provider_x509_cert_url":
+            dotenv.env['AUTH_PROVIDER_X509_CERT_URL'],
+        "client_x509_cert_url": dotenv.env['CLIENT_X509_CERT_URL'],
+        "universe_domain": dotenv.env['UNIVERSE_DOMAIN']
+      }),
+      scopes,
+    );
+    return client.credentials.accessToken.data;
   }
 }
